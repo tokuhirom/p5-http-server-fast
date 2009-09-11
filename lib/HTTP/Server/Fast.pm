86f31baaafb4e6cfa64e76248c591ad506797bdb
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-09-11 13:12:49 +0900

    initial commit

---
+++ lib/HTTP/Server/Fast.pm
@@ -0,0 +1,33 @@
+package HTTP::Server::Fast;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+HTTP::Server::Fast -
+
+=head1 SYNOPSIS
+
+  use HTTP::Server::Fast;
+
+=head1 DESCRIPTION
+
+HTTP::Server::Fast is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom @*(#RJKLFHFSDLJF gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
